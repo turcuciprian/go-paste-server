0a0658642c29a3b9160d5843bf090efdae70df56
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-29 23:20:15 +0300

    renamed functions

--- paste-server.go
+++ paste-server.go
@@ -21,19 +21,38 @@ type Address struct {
 
 var people []Person
 
-func GetPersonEndPoint(w http.ResponseWriter, req *http.Request) {
+// func getSpecificSLR(w http.ResponseWriter, req *http.Request) {
+// 	params := mux.Vars(req)
+// 	for _, item := range people {
+// 		if item.ID == params["id"] {
+// 			json.NewEncoder(w).Encode(item)
+// 			return
+// 		}
+// 	}
+// 	json.NewEncoder(w).Encode(&Person{})
 
-}
+// }
 
-func GetPeopleEndPoint(w http.ResponseWriter, req *http.Request) {
+func getSLR(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(people)
 
 }
-func CreatePersonEndPoint(w http.ResponseWriter, req *http.Request) {
-
+func addSLR(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	var person Person
+	_ = json.NewDecoder(req.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
-func DeletePersonEndPoint(w http.ResponseWriter, req *http.Request) {
-
+func delSLR(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+		}
+	}
+	json.NewEncoder(w).Encode(people)
 }
 
 //
@@ -43,10 +62,9 @@ func main() {
 	router := mux.NewRouter()
 	people = append(people, Person{ID: "1", Firstname: "Nic", LastName: "Raboy", Address: &Address{City: "Dublin", State: "California"}})
 	people = append(people, Person{ID: "2", Firstname: "Maria", LastName: "Raboy"})
-	router.HandleFunc("/people", GetPeopleEndPoint).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPersonEndPoint).Methods("GET")
-	router.HandleFunc("/people/{id", CreatePersonEndPoint).Methods("POST")
-	router.HandleFunc("/people/{id", DeletePersonEndPoint).Methods("DELETE")
+	router.HandleFunc("/secretLinkRoute", getSLR).Methods("GET")
+	router.HandleFunc("/slrAdd/{id}", addSLR).Methods("POST")
+	router.HandleFunc("/slrDelete/{i}", delSLR).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
