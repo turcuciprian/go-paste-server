c3f32a16ad7f3918e1800d8f9dba083fa327bec3
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-29 19:51:26 +0300

    added basic routing

--- paste-server.go
+++ paste-server.go
@@ -1,6 +1,51 @@
 package main
-import "fmt"
 
-func main(){
-	fmt.Println("yeye!")
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type Todo struct {
+	Name      string
+	Completed bool
+	Due       time.Time
+}
+
+type Todos []Todo
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+//this does nothing much
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+//todoindex  returns json
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	json.NewEncoder(w).Encode(todos)
+}
+
+// TodoShow shows specific index
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
 }
