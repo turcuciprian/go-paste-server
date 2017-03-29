c0131683f762a97df0119955fdfc463f2c52861d
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-29 23:24:25 +0300

    renamed variables

--- paste-server.go
+++ paste-server.go
@@ -8,51 +8,46 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Person struct {
-	ID        string   `json:"id,omitempty"`
-	Firstname string   `json:"firstname,omitempty"`
-	LastName  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
-}
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
+type codeData struct {
+	ID         string `json:"id,omitempty"`
+	secretCode string `json:"secretCode,omitempty"`
 }
 
-var people []Person
+var codes []codeData
 
 // func getSpecificSLR(w http.ResponseWriter, req *http.Request) {
 // 	params := mux.Vars(req)
-// 	for _, item := range people {
+// 	for _, item := range codes {
 // 		if item.ID == params["id"] {
 // 			json.NewEncoder(w).Encode(item)
 // 			return
 // 		}
 // 	}
-// 	json.NewEncoder(w).Encode(&Person{})
+// 	json.NewEncoder(w).Encode(&codeData{})
 
 // }
 
 func getSLR(w http.ResponseWriter, req *http.Request) {
-	json.NewEncoder(w).Encode(people)
+	json.NewEncoder(w).Encode(codes)
 
 }
 func addSLR(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
-	var person Person
-	_ = json.NewDecoder(req.Body).Decode(&person)
-	person.ID = params["id"]
-	people = append(people, person)
-	json.NewEncoder(w).Encode(people)
+	var codeData codeData
+	_ = json.NewDecoder(req.Body).Decode(&codeData)
+	codeData.ID = params["id"]
+	codes = append(codes, codeData)
+	json.NewEncoder(w).Encode(codes)
 }
+
 func delSLR(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
-	for index, item := range people {
+	for index, item := range codes {
 		if item.ID == params["id"] {
-			people = append(people[:index], people[index+1:]...)
+			codes = append(codes[:index], codes[index+1:]...)
 		}
 	}
-	json.NewEncoder(w).Encode(people)
+	json.NewEncoder(w).Encode(codes)
 }
 
 //
@@ -60,8 +55,9 @@ func delSLR(w http.ResponseWriter, req *http.Request) {
 //
 func main() {
 	router := mux.NewRouter()
-	people = append(people, Person{ID: "1", Firstname: "Nic", LastName: "Raboy", Address: &Address{City: "Dublin", State: "California"}})
-	people = append(people, Person{ID: "2", Firstname: "Maria", LastName: "Raboy"})
+	//one and only code
+	codes = append(codes, codeData{ID: "1", secretCode: "Empty"})
+
 	router.HandleFunc("/secretLinkRoute", getSLR).Methods("GET")
 	router.HandleFunc("/slrAdd/{id}", addSLR).Methods("POST")
 	router.HandleFunc("/slrDelete/{i}", delSLR).Methods("DELETE")
