ef329b67e51c50725ae631e222dac5ae6cf80044
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-29 21:48:14 +0300

    made things simple

--- paste-server.go
+++ paste-server.go
@@ -1,53 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+	"io"
 	"net/http"
-	"time"
-
-	"github.com/gorilla/mux"
 )
 
-//Todo structure
-type Todo struct {
-	Name      string
-	Completed bool
-	Due       time.Time
+func foo(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "foo Ran")
 }
-
-// Todos structure array
-type Todos []Todo
 
 func main() {
-
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/todos", TodoIndex)
-	router.HandleFunc("/todos/{todoId}", TodoShow)
-
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-//Index this does nothing much
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome!")
-}
-
-//TodoIndex  returns json
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Host meetup"},
-	}
-
-	json.NewEncoder(w).Encode(todos)
-}
-
-// TodoShow shows specific index
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo show:", todoId)
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":8000", nil)
 }
